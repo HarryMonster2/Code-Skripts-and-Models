43387b0023ab4306639667896bb9c95267846fab
HarryMonster <56574574+HarryMonster2@users.noreply.github.com>
2022-05-06 08:51:09 +1200

    2

--- Skripts/player/cam/Look.cs
+++ Skripts/player/cam/Look.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class Look : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
-    public Transform head;
-    float xRotation = 0f;
-    // Start is called before the first frame update
-    void Start()
-    {
+    [Range(30, 200)]
+    [SerializeField] private float sensitivity;
+
+    [SerializeField] private Transform playerBody;
+
+    private float xRotation = 0f;
+
+	private void Start()
+	{
         Cursor.lockState = CursorLockMode.Locked;
-    }
-    // Update is called once per frame
-    void Update()
+	}
+
+	// Update is called once per frame
+	void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        xRotation = Mathf.Clamp(xRotation - mouseY, -90f, 90f);
-        head.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        transform.Rotate(Vector3.up * mouseX);
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
